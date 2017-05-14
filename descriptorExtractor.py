5ea150784edb9c0290d9bdb64c11bfe58925eeaa
Adeykin <adeykin90@gmail.com>
2017-05-14 22:03:05 +0300

    HOG descriptor extractor

--- descriptorExtractor.py
+++ descriptorExtractor.py
@@ -2,43 +2,53 @@
 Descriontion: Extract descriptors from examples and save it to .csv file
 """
 
-import os
-import cv2
-import numpy as np
 import csv
+import math
+import numpy as np
+import cv2
+import os.path
+import common
+import markParser
 
-inputDir = '/home/adeykin/projects/parking/115000004/images/1/negativeExtend'
-output = '/home/adeykin/projects/parking/115000004/images/1/negativeExtend.csv'
-
-"""
-inputDir = '/home/adeykin/projects/parking/115000004/images/2/negative'
-output = '/home/adeykin/projects/parking/115000004/images/2/negative.csv'
-"""
+inputDir = "/home/adeykin/projects/parking/data/115000004/901000011_9"
+listPath = "/home/adeykin/projects/parking/data/115000004/901000011_9/positiveSmallMasked.txt"
+output = "/home/adeykin/projects/parking/data/115000004/901000011_9/positiveSmallMasked.csv"
 
 f = open(output, 'w')
 spamwriter = csv.writer(f, delimiter=';')
 
-for file in os.listdir(inputDir):
-    img = cv2.imread(inputDir + '/' + file, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    
-    winSize = (64,64)
-    blockSize = (16,16)
-    blockStride = (8,8)
-    cellSize = (8,8)
-    nbins = 9
-    derivAperture = 1
-    winSigma = 4.
-    histogramNormType = 0
-    L2HysThreshold = 2.0000000000000001e-01
-    gammaCorrection = 0
-    nlevels = 64
-    hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
-                            histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+size = 40,120
+#locations = ((20,60))
 
-    winStride = (8,8)
-    padding = (8,8)
-    locations = ((10,20),)
-    hist = hog.compute(img,winStride,padding,locations)
+locations = ((10,20),)    
+winSize = size
+blockSize = (16,16)
+blockStride = (8,8)
+cellSize = (8,8)
+nbins = 9
+
+hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
+
+parser = markParser.MarkParser()
+parser.load(listPath)
+
+#hog = cv2.HOGDescriptor(winSize = size, nbins = 9)
+
+for mark in parser.marks:
+    img = cv2.imread(inputDir + '/' + mark.imageName, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    #imgNameBase = os.path.splitext(mark.imageName)[0]
+    #i = 0
+    crops = []
+    for poligon in mark.poligons:
+        crop = common.extractCrop(img, poligon, size)
+        crops.append( crop )
+        crops.append( cv2.flip(crop, 0) )
+        crops.append( cv2.flip(crop, 1) )
+        crops.append( cv2.flip(crop, -1) )
+        
+    for crop in crops:
+        hist = hog.compute(crop)
+        spamwriter.writerow(hist.transpose().tolist()[0])
     
     #print hist.transpose()
     
@@ -47,7 +57,7 @@ for file in os.listdir(inputDir):
     
     #print hist.transpose().tolist()
     #print 'Size of list to write: ' + str(len(hist.transpose().tolist()[0])) 
-    spamwriter.writerow(hist.transpose().tolist()[0])
+    
     
     #np.savetxt(f, hist.transpose(), delimiter=';')
     #cv2.waitKey()
